dc772080365e628fa9d4add7cc5865f86a8e182d
geran1ka <roman.khoruzhy@gmail.com>
2023-05-29 00:08:07 +0300

    modal

--- script/modules/modalControl.js
+++ script/modules/modalControl.js
@@ -5,6 +5,3 @@ const headerLinkCall = document.querySelector('.header__link-call');
 headerLinkCall.addEventListener('click', () => {
   overlay.classList.add('overlay_active');
 });
-
-const input = document.querySelector('.modal__input')
-console.dir('input: ', input);
