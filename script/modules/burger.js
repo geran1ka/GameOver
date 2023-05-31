18000ef7919427c4466698e68236a6bb9eafd62c
geran1ka <roman.khoruzhy@gmail.com>
2023-05-31 22:28:14 +0300

    lauoyt header ready

--- script/modules/burger.js
+++ script/modules/burger.js
@@ -1,34 +1,31 @@
-import {nav, navigationList, page} from './const.js';
-import {createElement} from './function.js';
+const headerMain = document.querySelector('.header');
+const burger = document.querySelector('.burger');
+const navList = document.querySelector('.navigation__list');
+const navMenu = document.querySelector('.navigation');
+const headerOverlay = document.querySelector('.header__overlay');
 
-
-export const createBurgerMenu = () => {
-  const burger = createElement('button', {
-    className: 'navigation__menu',
-    innerHTML: '<span class="navigation__line"></span>',
-  });
-
-  burger.addEventListener('click', (e) => {
-    burger.classList.toggle('navigation__menu_active');
-    navigationList.classList.toggle('navigation__list_active');
-  });
-
-  nav.prepend(burger);
-
-  return burger;
-};
-/*
-const burger = createBurgerMenu();
-
-
-page.addEventListener('click', e => {
-  const target = e.target;
-
-  if (!(navigationList.classList.contains('navigation__list_active') &&
-    target === navigationList) && target !== burger) {
-    navigationList.classList.remove('navigation__list_active');
-    burger.classList.remove('navigation__menu_active');
+burger.addEventListener('click', e => {
+  if (burger.classList.contains('burger_active')) {
+    burger.classList.remove('burger_active');
+    navList.classList.remove('navigation__list_active');
+    navMenu.classList.remove('navigation_active');
+    headerOverlay.classList.remove('header__overlay_active');
+  } else {
+    burger.classList.add('burger_active');
+    navList.classList.add('navigation__list_active');
+    navMenu.classList.add('navigation_active');
+    headerOverlay.classList.add('header__overlay_active');
   }
 });
 
-*/
+headerMain.addEventListener('click', e => {
+  const target = e.target;
+  console.log('target: ', target);
+
+  if (target.closest('header') && target !== burger) {
+    burger.classList.remove('burger_active');
+    navList.classList.remove('navigation__list_active');
+    navMenu.classList.remove('navigation_active');
+    headerOverlay.classList.remove('header__overlay_active');
+  }
+});
