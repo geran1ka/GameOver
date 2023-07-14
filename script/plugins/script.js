a68cebb80ef8258e5e48dc990014fc6e88f4c640
geran1ka <roman.khoruzhy@gmail.com>
2023-07-14 06:44:53 +0300

    интеракивная карта

--- script/plugins/script.js
+++ script/plugins/script.js
@@ -109,18 +109,32 @@ new Swiper('.swiper', {
 
 // const getCart = async () => {
 //   await ymaps3.ready;
+//   const {
+//     YMap,
+//     YMapDefaultSchemeLayer,
+//     YMapMarker,
+//     YMapControls,
+//     YMapDefaultFeaturesLayer
+// } = ymaps3;
 
 //   // Создание карты
 //   const map = new ymaps3.YMap(document.getElementById('map'), {
 //     location: {
-//       center: [55.723151, 37.565021],
-//       zoom: 10,
+//       center: [37.565084, 55.723157],
+//       zoom: 17,
 //     },
 //   });
-//   map.addChild(new ymaps3.YMapDefaultSchemeLayer());
+//   // map.addChild(new ymaps3.YMapDefaultSchemeLayer());
+//   map.addChild(scheme = new YMapDefaultSchemeLayer());
+//   map.addChild(new YMapControls({position: 'right'}));
+//   map.addChild(new YMapDefaultFeaturesLayer({id: 'features'}));
+//   const marker = new YMapDefaultMarker([37.565084, 55.723157]);
+//   map.addChild(marker);
 // };
 
 // getCart();
+
+
 
 ymaps.ready(init);
 
@@ -134,7 +148,19 @@ function init() {
     hintContent: 'GAME OVER',
     balloonContent: 'GAME OVER',
   }, {
-  })
+  });
 
-  myMap.geoObjects.add(mark)
+  myMap.geoObjects.add(mark);
+
+  myMap.behaviors.disable('scrollZoom'); // запрещает зум карты колесиком
+  myMap.behaviors.disable('drag'); // запрещает скрол свайпом
+
+  myMap.controls.remove('geolocationControl'); // удаляет геолокацию (ваше местоположение)
+  myMap.controls.remove('searchControl'); // удаляет гепоиск
+  myMap.controls.remove('trafficControl'); // удаляет контроль трафика
+  myMap.controls.remove('typeSelector'); // удаляет тип
+  myMap.controls.remove('fullscreenControl'); // удаляет переход в полноэкранный режим
+  myMap.controls.remove('zoomControl'); // удаляет контроль зуммирования
+  myMap.controls.remove('rulerControl'); // удаляет контроль правил
+
 }
